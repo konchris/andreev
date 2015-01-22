a2c1455de20a6ed773a6f15df760b23be0a9c7c7
martin.2.keller <martin.2.keller@05e190a5-2555-4bbc-ac87-27ebbf8c4064>
2015-01-22 10:02:01 +0000

    ADD: YOKO7651 in devices_andreev.py
    git-svn-id: https://svn.uni-konstanz.de/ag-scheer/andreev@49 05e190a5-2555-4bbc-ac87-27ebbf8c4064

--- devices_andreev.py
+++ devices_andreev.py
@@ -592,6 +592,119 @@ class GS200:
         """returns the last set value"""
         return self.voltage
 
+class YOKO7651:
+    def __init__(self, GPIB_No=24):
+        self.yoko=visa.instrument('GPIB0::%i'%GPIB_No, delay=0.05, term_chars='\n')
+        self.ranges={10e-3:'R2', 100e-3:'R3', 1.0:'R4', 10.0:'R5', 30.0:'R6'}
+        self.SetPoint=None
+        self.status={'prog set':None, 'prog run':None, 'error':None, 'outp unstable':None, 'outp on':None, 'calib mode': None, 'memory card':None, 'CAL switch':None}
+        self.byteorder=['CAL switch', 'memory card', 'calib mode', 'outp on', 'outp unstable', 'error', 'prog run', 'prog set']
+        self.initialize()
+   
+    def initialize(self):
+        """get current set point, set default parameters, ranges etc"""        
+        self.yoko.write('RC')
+        self.yoko.write('o1e')
+        self.set_voltage(0.001)
+    
+    def set_range(self, value):
+        """set appropriate range for target value"""
+        rangevals=np.array(self.ranges.keys())
+        rangecomm=self.ranges[min(rangevals[np.where(rangevals>=float(value))])]
+        self.yoko.write('%s'%rangecomm)
+        self.yoko.write('E')
+
+    def set_voltage(self, U=1.0e-6):
+        """define yoko setpoint U in V"""
+        self.voltage=float(U)
+        self.yoko.write('S%f'%float(U))
+        print 'S%f'%float(U)
+        self.yoko.write('E')
+    
+    def output(self,state=True):
+        """defines the state of the voltage output.
+        possible: True=ON | False=OFF"""
+        if state:
+            self.yoko.write("o1e")
+        else:
+            self.yoko.write("o1e")
+
+    def display_set_text(self, text="GS200"):
+        """Displays desired text"""
+        pass
+    def display_main_screen(self):
+        """Sets Display back to normal"""
+        pass
+   
+    def program_is_end(self):
+        try:
+            status = self.yoko.ask("OC")
+            #print status
+            if int(status.split("=")[-1]) & (1 << 1): # program being executed
+                # 1 = running
+                return False
+            else:
+                return True
+        except Exception,e:
+            log("Yoko program_is_end",e)
+            return False
+
+    def program_goto_ramp(self, voltage=0.0, slope_time=1):
+        """set the sweep parameters for yokogawa
+           !!! the sweep is performed starting from the actual value to the first value in Bias_step !!!
+
+           Bias_step=[2.0, -2.0] - a list of bias target voltages in the sweep
+           step_interval=120 - the time interval between two subsequent steps the sweep
+           step_sweep=120  -  the duration of sweeping the voltage between two subseqent steps
+           
+           NOTE: step_interval should be equal or larger than step_sweep
+                 if step_interval=step_sweep the voltage source produces a triangular sawtooth function
+                 if step_interval>step_sweep the voltage source produces a ramp + dwell function
+                 if step_interval>0 and  step_sweep=0 the voltage source produces a rectangular staircase function
+                 make sure to have no range switch inbetween (david)
+        """
+        #migration david->scheerIV
+        Bias_step = [voltage]
+        step_interval = slope_time
+        step_sweep = slope_time
+        #migration david->scheerIV
+        
+        maxbias=max([abs(i) for i in Bias_step])        
+        rangevals=np.array(self.ranges.keys())
+        rangecomm=self.ranges[min(rangevals[np.where(rangevals>=maxbias)])]
+        self.yoko.write('%s'%rangecomm)
+
+        #yoko.yoko.write('S%f'%float(Bias_step[-1])) #setpoint -> start sweep at the same where sweep finishes
+        self.yoko.write('E')    #trigger
+        self.yoko.write('PRS')  #open programming mode
+        self.yoko.write('F1')   #set V:DC source
+        self.yoko.write('%s'%rangecomm)
+        for i in Bias_step:     #create program steps
+            self.yoko.write('S%f'%float(i)) 
+            time.sleep(0.1)
+        self.yoko.write('PRE')  #end programming mode
+        self.yoko.write('PI%f'%float(step_interval))    #set step interval time
+        self.yoko.write('E')
+        self.yoko.write('SW%f'%float(step_sweep))       #set sweep interval time
+        self.yoko.write('E') 
+        
+        self.yoko.write('M1')   #single
+        self.yoko.write('E')
+        self.yoko.write('RU2')
+        
+
+    def set_function(self,function="VOLT"):
+        """sets the function of the device.
+        possible: VOLT | CURR"""
+        pass
+    
+    def set_limits(self, current=0.005, voltage=2.000):
+        """sets the limits for voltage and current"""
+        pass
+
+    def get_voltage(self):
+        """returns the last set value"""
+        return self.voltage
 
 class Agilent34410A:
     def __init__(self, label="Agilent", dlay=0, addr="USB0::0x0957::0x0607::MY47030989::0::INSTR",timeout=0.2): #GPIB_No=22, 
