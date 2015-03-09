730a55130e18137ff865a74dac8a0e5d9e431e25
david.weber <david.weber@05e190a5-2555-4bbc-ac87-27ebbf8c4064>
2015-03-09 15:38:28 +0000

    git-svn-id: https://svn.uni-konstanz.de/ag-scheer/andreev@58 05e190a5-2555-4bbc-ac87-27ebbf8c4064

--- config.py
+++ config.py
@@ -1,6 +1,54 @@
+import os
 
-""" Defines which driver to load"""
-installed_yoko = "GS200" # "GS200" or "7651"
+userList = os.listdir('C:\\Users')
 
-installed_agilent_voltage = "USB0::0x0957::0x0607::MY47030989::0::INSTR"
-installed_agilent_current = "USB0::0x0957::0x0607::MY47031049::0::INSTR"
+for i in range(len(userList)):
+    if userList[i] == '1K_Stick_Clone':
+        userIndex = 2
+        break
+    elif userList[i] == 'David Weber': #test
+        userIndex = 1
+        break
+    else:
+        print 'Can not detect defined User.'
+        
+
+if userIndex == 2:
+    """ Defines which yoko driver to load"""
+    installed_yoko = "7651"
+
+    """ Defines weather to save txt files as well"""
+    save_good_old_txt = False
+
+    """ Defines the addresses of the different devices."""
+    installed_agilent_current = "USB0::0x0957::0x0607::MY53008595::0::INSTR"
+    installed_agilent_voltage = "USB0::0x0957::0x0607::MY53006248::0::INSTR"
+    
+    """ Motor address"""
+    installed_motor = "COM1"
+    
+    """ Definitions which magnets should be started and the address"""
+    magnetZ = True
+    magnetX = True
+    magnetZAddresse = 26
+    magnetXAddresse = 25
+    
+if userIndex == 1:
+    """ Defines which yoko driver to load"""
+    installed_yoko = "GS200"
+
+    """ Defines weather to save txt files as well"""
+    save_good_old_txt = False
+
+    """ Defines the addresses of the different devices."""
+    installed_agilent_current = "USB0::0x0957::0x0607::MY47031049::0::INSTR"
+    installed_agilent_voltage = "USB0::0x0957::0x0607::MY47030989::0::INSTR"
+    
+    """ Motor address"""
+    installed_motor = "ASRL3"
+    
+    """ Definitions which magnets should be started"""
+    magnetZ = False
+    magnetX = False
+    magnetZAddresse = 26
+    magnetXAddresse = 25    
