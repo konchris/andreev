2d5eda670239cd3835336e4069943a7b9ab4cd48
david.weber <david.weber@05e190a5-2555-4bbc-ac87-27ebbf8c4064>
2015-03-24 18:23:33 +0000

    git-svn-id: https://svn.uni-konstanz.de/ag-scheer/andreev@60 05e190a5-2555-4bbc-ac87-27ebbf8c4064

---
+++ analysis/analyze_switching.py
@@ -0,0 +1,451 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 19 21:02:03 2014
+
+@author: David Weber
+"""
+
+#import tables
+import numpy as np
+import os
+import time
+import datetime
+import pylab as pl
+import scipy.constants as const
+
+from functions_evaluate import *
+import MAR_functions
+
+    
+r_0 = const.h/const.e**2/2.0
+g_0 = 2.0*const.e**2/const.h
+################################
+##### START ####################
+################################
+
+db = 0
+
+
+filename = r"150312_Pb314_Histo03"
+
+base_path = os.path.join("Z:\dweber\data_p5",filename)              # server
+base_path = os.path.join("C:\data_p5",filename)                     # local
+base_path = r"D:\Desktop\150314_Pb314_Switching06"   # custom
+total_path = [os.path.join(base_path,'db_%i.h5'%(db))]
+config_path = os.path.join(base_path,'config.txt')
+measurement_switching = "switching_1426339545.txt"
+
+
+pl.close("all")
+
+skip_loading = False
+if 'old_filename' in locals():
+    if filename == old_filename:
+        if last_loading_successfull:
+            skip_loading = True
+
+old_filename = filename
+_time = time.time()
+
+if skip_loading:
+    print "Skipping Loading"
+    last_loading_successfull = True
+else:
+    last_loading_successfull = False
+    print "Loading %s"%total_path
+
+if not last_loading_successfull:
+    data_packages = []
+    for file_total in total_path:
+        data_packages.append(HDF_DATA(file_total))
+    last_loading_successfull = True
+    
+
+###########################################################################
+# SPLIT UP SPLIT UP SPLIT UP SPLIT UP SPLIT UP SPLIT UP SPLIT UP SPLIT UP #
+###########################################################################
+split_up_ivs = False
+try:  
+    # splitting up the config file for sweeps, histograms, etc.
+    print "Loading %s"%config_path
+    config = open(config_path)
+    config_rows = config.readlines()
+    
+    histograms = []
+    ivs = []
+    bcircle = []
+    bsweep = []
+    
+    last_iv_start = 0
+    last_iv_end = 0
+    last_hist_open = 0
+    last_hist_close = 0
+    last_circle_start = 0
+    last_circle_stop = 0
+    last_bsweep_start = 0
+    last_bsweep_stop = 0
+    for line in config_rows:
+        commands = line.split()
+        if commands[0] == "IV_START":
+            last_iv_start = float(commands[1])
+        if commands[0] == "IV_STOP":
+            last_iv_end = float(commands[1])
+            ivs.append([last_iv_start, last_iv_end]) 
+        
+        if commands[0] == "HISTOGRAM_OPEN":
+            last_hist_open = float(commands[1])
+            histograms.append([last_hist_close, last_hist_open])
+        if commands[0] == "HISTOGRAM_CLOSE":
+            last_hist_close = float(commands[1])
+            histograms.append([last_hist_open, last_hist_close])
+        
+        if commands[0] == "BCIRCLE_START":
+            last_circle_start = float(commands[1])
+        if commands[0] == "BCIRCLE_STOP":
+            last_circle_stop = float(commands[1])
+            bcircle.append([last_circle_start, last_circle_stop])
+        
+        if commands[0] == "BSWEEP_START":
+            last_bsweep_start = float(commands[1])
+        if commands[0] == "BSWEEP_STOP":
+            last_bsweep_stop = float(commands[1])
+            bcircle.append([last_bsweep_start, last_bsweep_stop])
+        
+    print "Found %i IVs, %i Traces, %i Circles, %i BSweeps"%(len(ivs),len(histograms),len(bcircle),len(bsweep))
+ 
+except Exception,e:
+    print e   
+    
+    
+##########################################################
+### Taking apart all measurements from bunch of data #####
+##########################################################
+
+if split_up_ivs:
+    # create dir and paths
+    iv_dir = os.path.join(base_path,"ivs")
+    trace_dir = os.path.join(base_path,"traces")
+    bsweep_dir = os.path.join(base_path,"bsweep")
+    ensure_dir(iv_dir+"\\")
+    ensure_dir(trace_dir+"\\")
+    ensure_dir(bsweep_dir+"\\")
+    print "IV-DIR: %s"%(iv_dir)
+    print "TRACE-DIR: %s"%(trace_dir)
+    print "BSWEEP-DIR: %s"%(bsweep_dir)
+    
+    # todo
+    seperate_windows = True
+    if not seperate_windows:
+        fig = pl.figure(figsize=(21,12), dpi=72)
+        fig.subplots_adjust(hspace = 0.3, wspace = 0.2)
+        ax_sub_a = fig.add_subplot(2,2,1) # fig.gca()
+        ax_didv = fig.add_subplot(2,2,2)
+        ax_sub_b = ax_didv.twinx()  
+        ax_sub_c = fig.add_subplot(2,2,3)
+        ax_sub_d = fig.add_subplot(2,2,4)
+      
+    i = 1
+    for iv_times in ivs[:]:
+        if not seperate_windows:
+            ax_sub_a.cla()
+            ax_didv.cla()
+            ax_sub_b.cla()
+            ax_sub_c.cla()
+            ax_sub_d.cla()
+            
+        i_begin = find_min(data_packages[0].x_list, iv_times[0])
+        i_end = find_min(data_packages[0].x_list, iv_times[1])
+
+        ax_x_factor = 1e3
+        ax_y_factor = 1e6
+        slice_voltage = data_packages[0].voltage[i_begin:i_end]*ax_x_factor
+        slice_current = data_packages[0].current[i_begin:i_end]*ax_y_factor
+        
+        fig = pl.figure(figsize=(21,12), dpi=72)
+        fig.subplots_adjust(hspace = 0.3, wspace = 0.2)
+        ax_sub_a = fig.add_subplot(2,2,1) # fig.gca()
+        ax_didv = fig.add_subplot(2,2,2)
+        ax_sub_b = ax_didv.twinx()
+        ax_sub_c = fig.add_subplot(2,2,3)
+        ax_sub_d = fig.add_subplot(2,2,4)
+        
+        ####################   A (I/V)   ####################
+        from matplotlib.patches import Rectangle
+        extra = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0)  #empty rectangle for legend
+        
+        current_center_index = np.argmin(abs(slice_current))
+        
+        du,di,shift,corr_u,corr_i,anti_sym = MAR_functions.find_iv_offset([slice_voltage, slice_current], verbal=True)
+        
+        ax_sub_a.hold(True)
+        if max(abs(slice_voltage)) > 10.0: # IETS
+            ax_sub_a.plot(  slice_voltage, slice_current, 'r-', label="Raw")
+            ax_sub_a.plot(  slice_voltage, slice_current, 'k-', label="Offset Corrected")
+        else: # if MAR
+            pl.hold(True)
+            branch_a,    = ax_sub_a.plot(  flip_data_positive(slice_voltage[0:current_center_index]), flip_data_positive(slice_current[0:current_center_index]), 'r--')
+            branch_b,    = ax_sub_a.plot(  flip_data_positive(slice_voltage[current_center_index:]), flip_data_positive(slice_current[current_center_index:]), 'g--')
+            offset_corr, = ax_sub_a.plot(  corr_u, corr_i, 'k-')
+            anti_sym,    = ax_sub_a.plot(  corr_u, anti_sym, 'b--')
+            
+            ax_sub_a.legend([branch_a,branch_b,offset_corr,anti_sym,extra,extra],["A","B","Offset Corrected","Anti Sym","dU %f mV"%(du), "dI %f $\mu$A"%(di)],loc=2)
+        
+        
+        # save raw agilent data interpolated on agilent_voltage
+        t_raw,v_raw = data_packages[0].v_raw   
+        
+        j_begin = find_min(t_raw, iv_times[0])
+        j_end = find_min(t_raw, iv_times[1])
+        
+        t_raw,v_raw = t_raw[j_begin:j_end],v_raw[j_begin:j_end]
+        drop, i_raw = data_packages[0].i_raw 
+        i_raw = np.interp(t_raw, drop, i_raw)
+        np.savetxt(os.path.join(iv_dir,str(int(iv_times[1]))+".txt"), np.array([v_raw,i_raw]).transpose(), "%10.10f", "\t")
+        
+        
+        
+        
+        ####################   B   ####################
+        ax_didv.plot(   slice_voltage, data_packages[0].di_dv[i_begin:i_end], 'k-', label="$dI/dV$")
+        pl.hold(True)       
+        ax_sub_b.plot( slice_voltage, data_packages[0].ch_0_r[i_begin:i_end]/max(data_packages[0].ch_0_r[i_begin:i_end]), 'g-', label="ch 0 r")
+        ax_sub_b.plot( slice_voltage, data_packages[0].ch_1_r[i_begin:i_end]/max(data_packages[0].ch_1_r[i_begin:i_end]), 'r-', label="ch 1 r")
+        ax_sub_b.plot( slice_voltage, data_packages[0].ch_3_r[i_begin:i_end]/max(data_packages[0].ch_3_r[i_begin:i_end]), 'y-', label="ch 3 r")
+        ax_sub_b.plot( slice_voltage, data_packages[0].ch_4_r[i_begin:i_end]/max(data_packages[0].ch_4_r[i_begin:i_end]), 'b-', label="ch 4 r")
+        
+        ax_sub_b.legend()
+
+        #ax_sub_b.plot( slice_voltage, d2i_dv2[i_begin:i_end]/di_dv[i_begin:i_end], 'b-')
+        
+        ax_sub_a.set_xlabel("Voltage (mV)")
+        ax_sub_a.set_ylabel("Current (uA)")
+        ax_sub_a.set_title("IV") 
+        ax_sub_a.grid()
+        
+        ax_didv.set_xlabel("Voltage (mV)")
+        ax_didv.set_ylabel("dI/dV ($G_0$)")
+        ax_didv.set_title("LockIn")
+        ax_didv.grid()
+        
+        ax_sub_b.set_ylabel("Raw Value (a.u.)")
+        
+        std = np.average(data_packages[0].ch_3_r[i_begin:i_end]/data_packages[0].di_dv[i_begin:i_end])
+        #ax_sub_b.set_ylim([0,min(5*std,max(d2i_dv2[i_begin:i_end]/di_dv[i_begin:i_end]))])
+        
+        
+        ####################   C   ####################
+        diff_x = movingaverage(data_packages[0].voltage[i_begin:i_end]*ax_x_factor,25)
+        diff_y = movingaverage(data_packages[0].di_dv[i_begin:i_end],5)
+        d2idv2_num = np.diff(diff_x) / np.diff(diff_y)
+        ax_sub_c.plot(data_packages[0].voltage[i_begin:i_end-1]*ax_x_factor, d2idv2_num, label="num. diff")
+        ax_sub_c.grid()
+        
+        
+        
+        _xmin = min(data_packages[0].voltage[i_begin:i_end]*ax_x_factor)
+        _xmax = max(data_packages[0].voltage[i_begin:i_end]*ax_x_factor)
+        std = np.average(abs(d2idv2_num)*5)
+        ax_sub_c.set_xlim([_xmin*0.9, _xmax*0.9])
+        ax_sub_c.set_ylim([-std, +std])
+        
+        ax_sub_c.set_title("Lockin Second")
+        ax_sub_c.set_xlabel("Voltage (mV)")
+        ax_sub_c.set_ylabel("$d^2I/dV^2$ (a.u.)")
+        
+        
+        ####################   D   ####################
+        ax_sub_d.plot(slice_voltage,data_packages[0].ch_0_theta[i_begin:i_end], label="ch 0")
+        ax_sub_d.plot(slice_voltage,data_packages[0].ch_1_theta[i_begin:i_end], label="ch 1")
+        ax_sub_d.plot(slice_voltage,data_packages[0].ch_3_theta[i_begin:i_end], label="ch 3")
+        ax_sub_d.plot(slice_voltage,data_packages[0].ch_4_theta[i_begin:i_end], label="ch 4")
+        ax_sub_d.legend()
+        
+        ax_sub_d.set_title("Lockin Phase")
+        ax_sub_d.set_xlabel("Voltage (mV)")
+        ax_sub_d.set_ylabel("Phase ($^\circ$)")
+        
+        #pl.rcParams['legend.loc'] = 'best'
+        
+        capture_time = datetime.datetime.fromtimestamp(int(iv_times[1])).strftime('%d.%m.%Y %H:%M:%S')
+        fig.text(0.0, 0.0, "Measured: %s"%(capture_time), fontdict=None)
+
+        fig.savefig(os.path.join(iv_dir,str(int(iv_times[1]))+".png"))
+        print "%i/%i %s"%(i,len(ivs),capture_time) 
+        plt.draw()
+        plt.pause(0.001)
+        
+        i += 1
+    pl.close()
+    
+    i = 1
+    for hist_times in histograms[1:2]:
+        
+        local_t, local_g = data_packages[0].v_raw[0], 12906.0/data_packages[0].r_raw
+    
+        
+        i_begin =   find_min(local_t, hist_times[0])
+        i_end =     find_min(local_t, hist_times[1])
+
+        fig = pl.figure(figsize=(12,12), dpi=72)
+        fig.subplots_adjust(hspace = 0.35, wspace = 0.6)
+        ax_cond = fig.add_subplot(1,1,1) 
+
+        
+        opening_data = []
+        closing_data = []
+        
+        ##############################
+        cond_lower_end = 1e-4
+        cond_higher_end = 1e1
+        ##############################
+        
+        if data_packages[0].r_raw[i_begin] > data_packages[0].r_raw[i_end]: 
+            # opening curve
+            opening_data.extend(data_packages[0].cond[i_begin:i_end])
+            j = i_begin
+            while j < i_end:
+                if data_packages[0].cond[j] < cond_higher_end:
+                    break
+                j += 1
+            k = j
+            while k < i_end:
+                if data_packages[0].cond[k] < cond_lower_end:
+                    break
+                k += 1
+            ax_cond.plot(data_packages[0].position[i_begin:i_end]*(-1.0),data_packages[0].cond[i_begin:i_end], 'k-')
+            ax_cond.set_title("Opening") 
+        else:
+            # closing curve
+            opening_data.extend(data_packages[0].cond[i_begin:i_end])
+            j = i_begin
+            while j < i_end:
+                if data_packages[0].cond[j] > cond_lower_end:
+                    break
+                j += 1
+            k = j
+            while k < i_end:
+                if data_packages[0].cond[k] > cond_higher_end:
+                    break
+                k += 1
+            ax_cond.plot(data_packages[0].position[i_begin:i_end],data_packages[0].cond[i_begin:i_end], 'k-')
+            
+            ax_cond.set_title("Closing") 
+
+
+        
+        ax_cond.set_xlabel("Pos (Turns)")
+        ax_cond.set_ylabel("S ($G_0$)")
+        ax_cond.set_yscale("log")
+        ax_cond.set_ylim([1e-4,1e2])
+        ax_cond.grid()
+        
+        fig.savefig(os.path.join(trace_dir,str(int(hist_times[1]))+".png"))
+        pl.close()
+        #trace_file = open(os.path.join(trace_dir,str(int(hist_times[1]))+".txt"),"w")
+        #save_data(trace_file, [position[i_begin:i_end],cond[i_begin:i_end]])
+        
+        print "%i/%i %i"%(i,len(histograms),int(hist_times[1]))
+        i += 1
+    
+    i = 1
+    for circle_times in bcircle[0:]:
+        
+        i_begin =   find_min(x_list, circle_times[0])
+        i_end =     find_min(x_list, circle_times[1])
+
+        fig = pl.figure(figsize=(12,12), dpi=72)
+        fig.subplots_adjust(hspace = 0.35, wspace = 0.6)
+        ax_angle = fig.add_subplot(2,1,1) # fig.gca()
+        ax_angle_2 = fig.add_subplot(2,1,2)
+        #ax_sub_b = ax_didv.twinx()
+        
+        np.seterr(divide="print")
+        angles = np.rad2deg(np.arctan(magnet_z[i_begin:i_end]/magnet_x[i_begin:i_end]))
+        
+        window_size=50
+        window = np.ones(int(window_size))/float(window_size)
+        smooth = np.convolve(cond, window, 'same')
+
+        ax_angle.plot(angles,cond[i_begin:i_end], 'k-')
+        ax_angle_2.plot(angles,smooth[i_begin:i_end], 'k-')
+        
+        ax_angle.set_xlabel("Degree")
+        ax_angle.set_ylabel("S (A/V)")
+        ax_angle.set_title("Circle") 
+        ax_angle.grid()
+        ax_angle.set_ylim([2,2.25])
+        
+        ax_angle_2.set_xlabel("Degree")
+        ax_angle_2.set_ylabel("S (A/V)")
+        ax_angle_2.set_title("Circle") 
+        ax_angle_2.grid()
+        ax_angle_2.set_ylim([2,2.25])
+        
+
+        fig.savefig(os.path.join(magnet_dir,str(int(circle_times[1]))+"_circle.png"))
+        pylab.close()
+        
+        print "%i/%i %i"%(i,len(bcircle),int(circle_times[1]))
+        i += 1
+        
+
+
+
+##########
+# SWITCHES #
+##########
+if True:
+    
+    sw_file = open(os.path.join(base_path, measurement_switching),"r")
+    
+    x_high,y_high = [],[]
+    x_low,y_low = [],[]
+    for row in sw_file.readlines():
+        a = row.split(",")
+        if len(a) == 5:
+            
+            
+            if a[1] == "high":
+                x_high.append(int(a[0]))
+                x_high.append(int(a[0]))
+                y_high.append(float(a[2]))
+                y_high.append(float(a[3]))
+            if a[1] == "low":
+                x_low.append(int(a[0])+0.5)
+                x_low.append(int(a[0])+0.5)
+                y_low.append(float(a[2]))
+                y_low.append(float(a[3]))
+    
+    
+    sw_plot = pl.figure(figsize=(8,6),dpi=120)
+    sw_plot.subplots_adjust(hspace = 0.4, wspace = 0.25)
+    
+    length = len(x_high)
+    plot_rows = 5    
+    for i in range(plot_rows):
+        sw_sub = sw_plot.add_subplot(plot_rows,1,i+1)
+           
+        start,end=length/plot_rows*i,length/plot_rows*(i+1)
+        #print start,end
+        sw_sub.plot(x_high[start:end],y_high[start:end],"^r",label="High Pulse")
+        sw_sub.plot(x_low[start:end],y_low[start:end],"*b",label="Low Pulse")
+        
+        sw_sub.set_xlim(start/2.0,end/2.0)
+        c = y_low + y_high
+        _max = max(c)*1.1
+        _min = min(c)*0.8
+        sw_sub.set_ylim([_min,_max])
+        
+        sw_sub.set_ylabel("G/$G_0$")
+        sw_sub.set_xlabel("Cycles")
+        #sw_sub.set_title("Switching Cycles (%i cycles)"%(len(x_high)))
+    #sw_sub.legend()
+
+    sw_plot.suptitle("Switching Cycles (%i cycles)"%(len(x_high)/2))
+    sw_plot.savefig(os.path.join(base_path,"switching.png"))
+    
+    sw_plot.show()
+
+
+    
+
--- analysis/plot_theory_curves_nicely_with_summation.py
+++ analysis/plot_theory_curves_nicely_with_summation.py
@@ -10,9 +10,11 @@ from MAR_functions import *
 import numpy as np
 
 dataset="t_4k_0_25"
-dataset2="t_0k"
+dataset="t_0k"
 
 pl.close("all")
+
+normalize = True
 
 
 #u,i,params = load_iv_data(r".\%s\%1.4f"%(dataset,(j+1)/10.0), index_u=0, index_i=1, skip=0)
@@ -29,31 +31,45 @@ pl.figure(figsize=(8,6),dpi=72)
 
 pl.hold(True)
 for j in range(len(transmissions)):
-    u,i,params = load_iv_data(r".\%s\%1.4f"%(dataset,transmissions[j]), index_u=0, index_i=1, skip=0)
+    if normalize:
+        u,i,params = load_iv_data(r".\%s\%1.4f"%(dataset,transmissions[j]), index_u=0, index_i=1, skip=0)
+        i = i/transmissions[j]
+        pl.text(3.2, i[int(2.0/u[-1]*len(u))]-0.2-j*0.01, "%1.1f"%(transmissions[j]))
+    else:
+        u,i,params = load_iv_data(r".\%s\%1.4f"%(dataset,transmissions[j]), index_u=0, index_i=1, skip=0)
+        pl.text(3.2, i[int(3.2/u[-1]*len(u))]-0.2-j*0.01, "%1.1f"%(transmissions[j]))
     pl.plot(u,i,color=colors[j%len(colors)], linewidth="3")    
     #pl.plot(u,i,label="%1.1f"%(transmissions[j]),color=colors[j%len(colors)], linewidth="2")    
-    pl.text(3.2, i[int(3.2/u[-1]*len(u))]-0.2-j*0.01, "%1.1f"%(transmissions[j]))
+    
 
 
-u,i,params = load_iv_data(r".\%s\%1.4f"%(dataset,0.2), index_u=0, index_i=1, skip=0)
-u2,i2,params = load_iv_data(r".\%s\%1.4f"%(dataset,0.3), index_u=0, index_i=1, skip=0)
-i = i+i2
-pl.plot(u,i,"b--",label="0.2+0.3", linewidth="4")
+#u,i,params = load_iv_data(r".\%s\%1.4f"%(dataset,0.2), index_u=0, index_i=1, skip=0)
+#u2,i2,params = load_iv_data(r".\%s\%1.4f"%(dataset,0.3), index_u=0, index_i=1, skip=0)
+#i = i+i2
+#pl.plot(u,i,"b--",label="0.2+0.3", linewidth="4")
 
 u,i,params = load_iv_data(r".\%s\%1.4f"%(dataset,0.4), index_u=0, index_i=1, skip=0)
 u2,i2,params = load_iv_data(r".\%s\%1.4f"%(dataset,0.6), index_u=0, index_i=1, skip=0)
-i = i+i2
+i = (i+i2)/1.0
 pl.plot(u,i,"g--",label="0.4+0.6", linewidth="4")
 
 pl.title("Theoretical I-V Curves MAR", fontsize=20)
 
-pl.xlim((0,3.5))
-pl.xticks([2.0/x for x in [0.666,1,2,3,4,5,6,7,8]],["3","2","1",r"$\frac{2}{3}$",r"$\frac{1}{2}$",r"$\frac{2}{5}$"])
-pl.xlabel("Voltage ($\Delta$)",fontsize=20)
+if normalize:
+    pl.xlim((0,3))
+else:
+    pl.xlim((0,3.5))
+pl.xticks([2.0/x for x in [0.666,1,2,3,4,5,6,7,8]],["3","2","1",r"$\frac{2}{3}$",r"$\frac{2}{4}$",r"$\frac{2}{5}$"])
+pl.xlabel("Voltage ($\Delta$/e)",fontsize=20)
 plt.gcf().subplots_adjust(left=0.08, bottom=0.15, right=0.99, top=0.95)
 
-pl.ylim((0,6))
-pl.ylabel("Current (2e$\Delta$/$\hbar$)",fontsize=20)
+
+if normalize:
+    pl.ylabel("Current (2e$\Delta$/G$\hbar$)",fontsize=20)
+    pl.ylim((0,6))
+else:
+    pl.ylim((0,6))
+    pl.ylabel("Current (2e$\Delta$/$\hbar$)",fontsize=20)
 legend = pl.legend(loc="best", title="Transmission", fontsize="16", ncol=1, shadow=True, )
 plt.setp(legend.get_title(),fontsize='16')
 pl.grid()
